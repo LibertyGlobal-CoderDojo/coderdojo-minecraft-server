929591eea8eafe91152104f982aef9f6bfad5547
Peter Halliday <pghalliday@gmail.com>
2016-03-03 23:57:08 +0100

    backup script

--- backup.sh
+++ backup.sh
@@ -6,10 +6,19 @@ PASSWD=$3
 ftp -n $HOST <<END_SCRIPT
 quote USER $USER
 quote PASS $PASSWD
-get *.json
-get *.yml
-get *.txt
-get *.properties
+get banned-ips.json
+get banned-players.json
+get bukkit.yml
+get commands.yml
+get config.yml
+get eula.txt
+get help.yml
+get ops.json
+get permissions.yml
+get server.properties
+get spigot.yml
+get usercache.json
+get whitelist.json
 quit
 END_SCRIPT
 exit 0
